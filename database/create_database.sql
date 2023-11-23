7c6c9848ca5c0fcab9c117fbaf0525f82d5aef69
Bannasorn Paspanthong <bannasornpaspanthong2023@u.northwestern.edu>
2023-11-23 15:07:35 -0600

    add table skills and majors

--- database/create_database.sql
+++ database/create_database.sql
@@ -29,9 +29,45 @@ CREATE TABLE students
     resume_s3   varchar(256),
     school_id   int,
     PRIMARY KEY (id),
-    UNIQUE      (email, linkedin, resume_s3),
+    UNIQUE      (email),
+    UNIQUE      (linkedin),
+    UNIQUE      (resume_s3),
     FOREIGN KEY (school_id) REFERENCES schools(id)
 )
 ALTER TABLE students AUTO_INCREMENT = 1;
 
+CREATE TABLE majors
+(
+    id          int not null AUTO_INCREMENT,
+    major      varchar(64) not null,
+    PRIMARY KEY (id),
+    UNIQUE      (major)
+)
+ALTER TABLE majors AUTO_INCREMENT = 1;
 
+CREATE TABLE student_major
+(
+    student_id  int not null,
+    major_id    int not null,
+    PRIMARY KEY (student_id, major_id),
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (major_id) REFERENCES majors(id)
+)
+
+CREATE TABLE skills
+(
+    id          int not null AUTO_INCREMENT,
+    skill       varchar(64) not null,
+    PRIMARY KEY (id),
+    UNIQUE      (skill)
+)
+ALTER TABLE skills AUTO_INCREMENT = 1;
+
+CREATE TABLE student_skill
+(
+    student_id  int not null,
+    skill_id    int not null,
+    PRIMARY KEY (student_id, skill_id),
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (skill_id) REFERENCES skills(id)
+)
